6b0248001ec216124fee4d879a1b619b10d8da74
Mrowr Purr <mrowr.purr@gmail.com>
2021-11-26 11:31:15 -0800

    Widgets are showing up given the correct paths

--- WebUI/WebUI/dist/webUI.js
+++ WebUI/WebUI/dist/webUI.js
@@ -4,7 +4,19 @@ var WebUIComponentHost = /** @class */ (function () {
     function WebUIComponentHost() {
     }
     WebUIComponentHost.prototype.show = function (component) {
-        alert("SHOW: ".concat(JSON.stringify(component)));
+        var iframe = document.createElement('iframe');
+        // iframe.style.display = 'none'
+        // iframe.contentWindow!.onload = () => {
+        //     iframe.style.display = 'block'
+        //     // call some callback on the SP side
+        // }
+        iframe.style.width = '500px';
+        iframe.style.height = '500px';
+        iframe.style.top = '0';
+        iframe.style.left = '0';
+        iframe.src = component.url;
+        document.documentElement.appendChild(iframe);
+        alert("Added ".concat(component.url));
     };
     return WebUIComponentHost;
 }());
--- WebUI/WebUI/webUI.ts
+++ WebUI/WebUI/webUI.ts
@@ -2,7 +2,19 @@ import { WebComponent } from 'skyrim-webui'
 
 class WebUIComponentHost {
     public show(component: WebComponent) {
-        alert(`SHOW: ${JSON.stringify(component)}`)
+        const iframe = document.createElement('iframe')
+        // iframe.style.display = 'none'
+        // iframe.contentWindow!.onload = () => {
+        //     iframe.style.display = 'block'
+        //     // call some callback on the SP side
+        // }
+        iframe.style.width = '500px'
+        iframe.style.height = '500px'
+        iframe.style.top = '0'
+        iframe.style.left = '0'
+        iframe.src = component.url
+        document.documentElement.appendChild(iframe)
+        alert(`Added ${component.url}`)
     }
 }
 
