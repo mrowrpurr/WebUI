fa8ee62928b9fc1c5c92b88c7eb517142734b19d
Mrowr Purr <mrowr.purr@gmail.com>
2021-11-28 10:04:33 -0800

    Can include script into JSDOM

--- src/UI/__tests__/hello.test.ts
+++ src/UI/__tests__/hello.test.ts
@@ -1,8 +1,14 @@
 import { JSDOM } from 'jsdom'
+import * as fs from 'fs'
 
 describe("testing", () => {
     it("something", () => {
-        const jsdom = new JSDOM('<h1>Hello</h1>')
+        const jsdom = new JSDOM('', { runScripts: 'dangerously', resources: 'usable', url: `file://${__dirname}/index.html` })
+        const script = jsdom.window.document.createElement('script')
+        script.textContent = fs.readFileSync('./foo.js').toString()
+        jsdom.window.document.documentElement.appendChild(script)
+        console.log(script.textContent)
+
         const h1 = jsdom.window.document.querySelector('h1')
         expect(h1?.textContent).toEqual("HELLO I AM AN H1")
     })
--- src/UI/jest.config.js
+++ src/UI/jest.config.js
@@ -2,5 +2,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  "roots": ["./__tests__/"]
+  "roots": ["./__tests__/"],
+  "testMatch": ["**/*.test.ts"]
 };
