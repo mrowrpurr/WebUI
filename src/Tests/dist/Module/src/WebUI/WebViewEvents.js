b7eb0e06f3084473de971836f584dbe707c690d5
Mrowr Purr <mrowr.purr@gmail.com>
2021-12-08 17:31:07 -0800

    Fresh start

--- src/Tests/dist/Module/src/WebUI/WebViewEvents.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/*
- * Skyrim Platform Backend
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=WebViewEvents.js.map
--- src/UI/dist/WebViewEvents.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/*
- * HTML Web Frontend
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=WebViewEvents.js.map
