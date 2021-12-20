2623ff604fe4bd6855aa17c636b98c79229ad349
Mrowr Purr <mrowr.purr@gmail.com>
2021-12-19 16:17:32 -0800

    Configured for jest tests

--- src/modDistribution/runtime/webpack.config.js
+++ src/modDistribution/runtime/webpack.config.js
@@ -1,26 +1,17 @@
 //webpack.config.js
 const path = require('path');
 const fs = require('fs')
-const skyrimInfo = require('../../../skyrim.config.js')
-const WebpackShellPluginNext = require('webpack-shell-plugin-next');
 const buildFolder = './build'
+const folderName = path.basename(__dirname)
 
 //////////////////////////////////
 // Configure this for each plugin
-const filename = 'WebUI.js'
+const filename = `${folderName}.js`
 //////////////////////////////////
-
-// This will run after webpack to deploy the plugin .js file to the Skyrim Platform plugin folder
-const deployScript = `xcopy "${path.join(buildFolder, filename)}" "${skyrimInfo.skyrimPluginsFolder}" /Y`
 
 module.exports = {
     mode: "development",
     devtool: "inline-source-map",
-    plugins: [
-        new WebpackShellPluginNext({
-            onBuildEnd: { scripts: [deployScript] }
-        })
-    ],
     entry: {
         main: "./src/index.ts",
     },
@@ -38,7 +29,10 @@ module.exports = {
         rules: [
             {
                 test: /\.tsx?$/,
-                loader: "ts-loader"
+                loader: "ts-loader",
+                options: {
+                    configFile: "tsconfig.build.json"
+                }
             }
         ]
     }
